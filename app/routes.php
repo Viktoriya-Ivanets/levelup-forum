ed8bab6511883af6a4e80c252fd01ce2b4aa1ea7
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-12-04 16:54:18 +0200

    Add new routes and missed documentor

--- app/routes.php
+++ app/routes.php
@@ -2,5 +2,10 @@
 
 use app\core\Router;
 
+/**
+ * Here you can define new routes
+ */
 Router::add('login', ['controller' => 'Auth', 'action' => 'login']);
+Router::add('logout', ['controller' => 'Auth', 'action' => 'logout']);
 Router::add('register', ['controller' => 'Auth', 'action' => 'register']);
+Router::add('', ['controller' => 'Category', 'action' => 'index']);
