c0c63d03c606e8429683d1f6914d45db4e4074e1
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-12-06 14:50:44 +0200

    Divide the category template into separate forms and use dynamic data generation

--- app/views/pages/categories.php
+++ app/views/pages/categories.php
@@ -1,52 +1,9 @@
 <div class="content-wrapper">
     <div class="content-header">
         <div class="container">
-            <div class="row mb-2">
-                <div class="col-6">
-                    <div class="col-12">
-                        <div class="card card-primary collapsed-card">
-                            <div class="card-header">
-                                <h1 class="card-title">Categories</h1>
-
-                                <div class="card-tools">
-                                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fa-solid fa-plus fa-2xl mt-3"></i>
-                                    </button>
-                                </div>
-                                <!-- /.card-tools -->
-                            </div>
-                            <!-- /.card-header -->
-                            <div class="card-body" style="display: none;">
-                                <div class="card card-primary">
-                                    <div class="card-header">
-                                        <h3 class="card-title">Create new category</h3>
-                                    </div>
-                                    <!-- /.card-header -->
-                                    <!-- form start -->
-                                    <form action="#" method="post">
-                                        <div class="card-body">
-                                            <div class="form-group">
-                                                <label for="title">Title</label>
-                                                <input type="text" name="title" class="form-control" id="title" placeholder="Enter title of category">
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Description</label>
-                                                <textarea class="form-control" name="description" rows="3" placeholder="Enter short description..."></textarea>
-                                            </div>
-                                        </div>
-                                        <!-- /.card-body -->
-
-                                        <div class="card-footer">
-                                            <button type="submit" class="btn btn-primary w-100">Submit</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            <!-- /.card-body -->
-                        </div>
-                        <!-- /.card -->
-                    </div>
-                </div>
-                <div class="col-6">
+            <div class="row mb-2 d-flex justify-content-between">
+                <h1 class="col-2">Categories</h1>
+                <div class="col-10">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item active">Categories</li>
                     </ol>
@@ -56,41 +13,38 @@
     </div>
     <div class="content">
         <div class="container">
+            <div class="row mb-3">
+                <div class="col-2">
+                    <a href="<?= \app\core\Router::url('categories/create') ?>" class="btn btn-primary">Add category</a>
+                </div>
+            </div>
             <div class="row">
-                <div class="col-lg-6 col-md-6">
-                    <div class="col-12">
-                        <div class="small-box bg-primary">
-                            <div class="inner p-3">
-                                <h3>PHP</h3>
-
-                                <p>Topics releated to PHP programming language</p>
+                <?php foreach ($categories as $category): ?>
+                    <div class="col-lg-6 col-md-6">
+                        <div class="col-12 p-0">
+                            <div class="small-box bg-primary">
+                                <div class="inner p-3">
+                                    <h3><?= htmlspecialchars($category['title']); ?></h3>
+                                    <p><?= htmlspecialchars($category['description']); ?></p>
+                                </div>
+                                <div class="icon">
+                                    <i class="fa-solid fa-list"></i>
+                                </div>
+                                <div class="small-box-footer p-2 d-flex justify-content-center">
+                                    <a href="#" class="text-white">See topics <i class="fas fa-arrow-circle-right ml-2"></i></a>
+                                    <?php if ($category['user_id'] === $user['id']): ?>
+                                        <a href="<?= app\core\Router::url('categories/edit/' . $category['id']) ?>" class="text-white ml-3">Edit <i class="fas fa-pencil ml-2"></i></a>
+                                        <a href="<?= app\core\Router::url('categories/delete/' . $category['id']) ?>" class="text-white ml-3">Delete <i class="fas fa-trash ml-2"></i></a>
+                                    <?php endif; ?>
+                                </div>
                             </div>
-                            <div class="icon">
-                                <i class="fa-solid fa-list"></i>
-                            </div>
-                            <a href="#" class="small-box-footer p-2">
-                                See topics <i class="fas fa-arrow-circle-right"></i>
-                            </a>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-6 col-md-6">
-                    <div class="col-12">
-                        <div class="small-box bg-primary">
-                            <div class="inner p-3">
-                                <h3>MySQL</h3>
-
-                                <p>Topics releated to MySQL </p>
-                            </div>
-                            <div class="icon">
-                                <i class="fa-solid fa-list"></i>
-                            </div>
-                            <a href="#" class="small-box-footer p-2">
-                                See topics <i class="fas fa-arrow-circle-right"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                    <?php if ($category['id'] % 2 === 0): ?>
+            </div>
+            <div class="row">
+            <?php endif; ?>
+        <?php endforeach; ?>
             </div>
         </div>
     </div>
---
+++ app/views/pages/category_edit.php
@@ -0,0 +1,58 @@
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container">
+            <div class="row mb-2 d-flex justify-content-between">
+                <div class="col-2">
+                    <h1>Edit category</h1>
+                </div>
+                <div class="col-10">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="<?= \app\core\Router::url('categories') ?>">Categories</a></li>
+                        <li class="breadcrumb-item active">Edit category</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Edit category</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <form action="<?= \app\core\Router::url('categories/update') ?>" method="post">
+                            <div class="card-body">
+                                <input type="hidden" name="id" value="<?= empty($old['id']) ? htmlspecialchars($category['id']) : htmlspecialchars($old['id']) ?>">
+                                <div class="form-group">
+                                    <label for="title">Title</label>
+                                    <input type="text" name="title" class="form-control" id="title" value="<?= empty($old['title']) ? htmlspecialchars($category['title']) : htmlspecialchars($old['title']) ?>" placeholder="Enter title of category">
+                                    <?php if (!empty($errors)): ?>
+                                        <div class="text-danger mt-2"><?= htmlspecialchars($errors['title']) ?></div>
+                                    <?php endif; ?>
+                                </div>
+                                <div class="form-group">
+                                    <label>Description</label>
+                                    <textarea class="form-control" name="description" rows="3" placeholder="Enter short description..."><?= empty($old['description']) ? htmlspecialchars($category['description']) : htmlspecialchars($old['description']) ?></textarea>
+                                    <?php if (!empty($errors)): ?>
+                                        <div class="text-danger mt-2"><?= htmlspecialchars($errors['description']) ?></div>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary w-100">Save</button>
+                                <a href="<?= app\core\Router::url('categories') ?>" class="btn btn-danger w-100 mt-2">Cancel</a>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
